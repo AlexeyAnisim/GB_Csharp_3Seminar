2754d4eb4a9341a0c26ff5a012441d4bf3744d82
Alex <al-an2017@rambler.ru>
2022-11-03 16:41:00 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿#nullable disable
+
+//Программа, которая выдает таблицу кубов от 1 до N
+Console.WriteLine("Введите число N"); 
+double num = double.Parse(Console.ReadLine());
+for(int i=1; i<=num; i++)
+{
+    Console.WriteLine($"{i*i*i}, ");
+}
