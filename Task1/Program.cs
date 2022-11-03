6b17f5fd110434bea924d2bd32e86e3f80621990
Alex <al-an2017@rambler.ru>
2022-11-04 01:12:31 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿#nullable disable
+
+// Программа проверки пятизначного числа на палиндром
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+
+int number1 = number.Length;
+
+if(number1 == 5)
+{
+    if(number[0] == number[4]  && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+      Console.WriteLine($"{number} - Не палиндром");  
+    }
+} 
+else
+{
+    Console.WriteLine($"Число: {number} - не пятизначное число");
+}
